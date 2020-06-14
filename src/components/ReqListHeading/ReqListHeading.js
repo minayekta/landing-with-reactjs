531b394ce92611d5396136b4d0ac801a453a6343
Mah-08 <mina.qolizadeh@hotmail.com>
2020-06-14 18:32:12 +0430

    first commit

--- src/components/ReqListHeading/ReqListHeading.js
+++ src/components/ReqListHeading/ReqListHeading.js
@@ -14,8 +14,8 @@ import image2 from "../../assets/images/Union 4.png"
                 <span className="icon"></span> <p className="p_icon"> Your email has been successfully registered, headlines will be sent to you soon.</p>           
             </div>
             <div className="div_img">
-                <img className="img1" src={image1}  />
-                <img className="img2" src={image2} />
+                <img className="img1" src={image1} alt="img"  />
+                <img className="img2" src={image2} alt="img2" />
                 
                 
                
